de84e882333812c2aca0409a4811263c80a0d04e
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2022-07-20 13:34:05 -0300

    Recebe frase por parametro, fonte Comic Sans e coloca borda no texto

--- src/Main.java
+++ src/Main.java
@@ -13,6 +13,8 @@ public class Main implements ConsoleColors {
 
     public static void main(String[] args) throws Exception {
 
+        Scanner sc = new Scanner(System.in);
+
         // Fazer uma conexão HTTP e buscar os top 250 filmes
         String url = "https://alura-imdb-api.herokuapp.com/movies";
         var endereco = URI.create(url);
@@ -25,6 +27,10 @@ public class Main implements ConsoleColors {
         ImdbApi items = gson.fromJson(body, ImdbApi.class);
         List <FilmApi> listFilm = List.of(items.getItems());
 
+        // Leitura da frase que irá aparecer nos stickers
+        System.out.printf(ANSI_BLUE + "\nInsira um texto para aparecer no seu sticker: ");
+        String phrase = sc.nextLine();
+
         // Exibir e manipular os dados
         System.out.println(ANSI_BLUE + "\n\t\t- - - - LISTA MELHORES FILMES - - - -");
         System.out.println(ANSI_BLUE + "Lista de filme classificadas conforme avaliação no site IMDB\n");
@@ -36,10 +42,8 @@ public class Main implements ConsoleColors {
                 String imgSmall = film.getImage();
                 String imgLarge = imgSmall.split("_" )[0] + "jpg";
                 InputStream inputStream = new URL(imgLarge).openStream();
-                makeFigure.create(inputStream, film.getTitle());
+                makeFigure.create(inputStream, film.getTitle(), phrase);
             } catch (Exception e) {
-                Scanner sc = new Scanner(System.in);
-
                 System.out.println(ANSI_RED  + "ATENÇÃO: imagem não encontrada!");
                 System.out.println(ANSI_RED  + "ENTER para Continuar");
                 sc.nextLine();
@@ -54,8 +58,6 @@ public class Main implements ConsoleColors {
             ***OBS: olhar na documentação o InputStream: https://docs.oracle.com/javase/7/docs/api/java/io/InputStream.html
             https://docs.oracle.com/javase/9/docs/api/java/io/InputStream.html
          */
-
-
 
     }
 }
--- src/MakeFigures.java
+++ src/MakeFigures.java
@@ -1,19 +1,19 @@
 import javax.imageio.ImageIO;
 import java.awt.*;
+import java.awt.font.TextLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
 
-
 public class MakeFigures implements ConsoleColors {
 
-    public void create(InputStream inputStream, String fileName) {
+    public void create(InputStream inputStream, String fileName, String phrase) {
 
         try {
             // leitura da imagem
             //InputStream inputStream = new FileInputStream(new File("entrada/filme.jpeg"));
-//            InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg").openStream();
+            //InputStream inputStream = new URL("https://m.media-amazon.com/images/M/MV5BMDFkYTc0MGEtZmNhMC00ZDIzLWFmNTEtODM1ZmRlYWMwMWFmXkEyXkFqcGdeQXVyMTMxODk2OTU@.jpg").openStream();
             BufferedImage original = ImageIO.read(inputStream);
 
             // cria nova imagem em memória com transparência e com tamanho novo
@@ -27,27 +27,33 @@ public class MakeFigures implements ConsoleColors {
             graphics.drawImage(original, 0, 0, null);
 
             // configurar a fonte
-            int fontSize = 80;
-            var font = new Font(Font.SANS_SERIF, Font.BOLD, fontSize);
+            int fontSize = height / 14;
+            var font = new Font("Comic Sans MS", Font.BOLD, fontSize);
             graphics.setColor(Color.YELLOW);
             graphics.setFont(font);
-            System.out.println();
 
-            // escrever uma frase em uma nova imagem
-            String phrase = "TOPZERA";
             // encontra o meio da imagem (eixo x)
             int posX = (int) ((width / 2) - (phrase.length() * (fontSize * 0.7)) / 2);
-            graphics.drawString(phrase, posX, (int) (newHeight - (newHeight * 0.1484)));
+            int posY = (int) (newHeight - (newHeight * 0.1484));
+            graphics.drawString(phrase, posX, posY);
+
+            // Faz contorno na imagem
+            graphics.setStroke(new BasicStroke((float) (height *  0.003)));
+            var textLayout = new TextLayout(phrase, font, graphics.getFontRenderContext());
+            var shape = textLayout.getOutline(null);
+            graphics.setColor(Color.BLACK);
+            graphics.translate(posX, posY);
+            graphics.draw(shape);
 
             // escrever a nova imagem em um arquivo
             fileName.concat(".png");
             File figure = new File("saida/"+fileName + ".png");
-//             Se não existir o diretório ele cria
-            if(figure.mkdirs())
+            // Se não existir o diretório ele cria
+//            if(figure.mkdirs())
                 ImageIO.write(newImage, "png", figure);
-            else {
-                System.out.println(ANSI_RED + "Imagem como nome já existente");
-            }
+//            else {
+//                System.out.println(ANSI_RED + "Imagem como nome já existente");
+//            }
 
         } catch (IOException e) {
             System.out.println("Não foi possível ler a imagem");
