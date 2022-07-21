177fd305fe725941cba3c8edd60e88a204fdd70c
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2022-07-21 11:41:57 -0300

    Coloca adesivo/foto ao lado da frase

--- src/MakeFigures.java
+++ src/MakeFigures.java
@@ -3,6 +3,7 @@ import java.awt.*;
 import java.awt.font.TextLayout;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -19,7 +20,7 @@ public class MakeFigures implements ConsoleColors {
             // cria nova imagem em memória com transparência e com tamanho novo
             int width = original.getWidth();
             int height = original.getHeight();
-            int newHeight = (int) (height + (height * 0.2968));
+            int newHeight = (int) (height + (height * 0.26));
             BufferedImage newImage = new BufferedImage(width, newHeight, BufferedImage.TRANSLUCENT);
 
             // copia a imagem original pra nova imagem (em memória)
@@ -27,18 +28,27 @@ public class MakeFigures implements ConsoleColors {
             graphics.drawImage(original, 0, 0, null);
 
             // configurar a fonte
-            int fontSize = height / 14;
+            phrase = phrase.toUpperCase();
+            int fontSize = height / 16;
             var font = new Font("Comic Sans MS", Font.BOLD, fontSize);
             graphics.setColor(Color.YELLOW);
             graphics.setFont(font);
 
             // encontra o meio da imagem (eixo x)
-            int posX = (int) ((width / 2) - (phrase.length() * (fontSize * 0.7)) / 2);
+            int posX = (int) ((width / 2) - (phrase.length() * (fontSize * 0.3)));
             int posY = (int) (newHeight - (newHeight * 0.1484));
             graphics.drawString(phrase, posX, posY);
 
+            // cria imagem/carimbo pessoal que será acrescentado
+            int resizeWidth, resizeHeight;
+            resizeWidth = resizeHeight = (int) (width * 0.28);
+            InputStream imgStamp = new FileInputStream(new File("stamp/hangLoose.png"));
+            BufferedImage bufferedImageInput = ImageIO.read(imgStamp);
+            graphics.drawImage(bufferedImageInput, width - resizeWidth, newHeight - resizeHeight,
+                    resizeWidth, resizeHeight, null);
+
             // Faz contorno na imagem
-            graphics.setStroke(new BasicStroke((float) (height *  0.003)));
+            graphics.setStroke(new BasicStroke((float) (height *  0.002)));
             var textLayout = new TextLayout(phrase, font, graphics.getFontRenderContext());
             var shape = textLayout.getOutline(null);
             graphics.setColor(Color.BLACK);
