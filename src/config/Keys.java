00c0cc1849852e2f7522b7579de3205ebcb19e3d
Irineu de Almeida Júnior <66287053+IrineuAlmeidaJr@users.noreply.github.com>
2022-07-25 00:33:09 -0300

    Create Keys.java

---
+++ src/config/Keys.java
@@ -0,0 +1,14 @@
+package config;
+
+public class Keys {
+    private final String PublicKey = "xxxx"; // Colocar aqui sua chave pública
+    private final String PrivateKey = "yyyy";  // Colocar aqui sua chave privada 
+
+    public String getPublicKey() {
+        return PublicKey;
+    }
+
+    public String getPrivateKey() {
+        return PrivateKey;
+    }
+}
